ae933cc028bab8b01b5eb6bdd51edef856e2eea4
Alejandro Germenos Ramirez <agermeno@opentext.com>
2018-05-31 15:29:03 -0700

    Initial commit

---
+++ src/main/java/com/opentext/communicator/domain/Producer.java
@@ -0,0 +1,11 @@
+/*
+ * Copyright (c) 2017 OpenText Corporation. All Rights Reserved.
+ * OpenText Confidential: Restricted Internal Distribution
+ */
+package com.opentext.communicator.domain;
+
+/*
+ * Created on 5/31/2018 by agermenos
+ */
+public class Producer {
+}
